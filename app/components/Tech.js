90994dde36b5e0947e4636e349bd2168a30facdc
Kunal Zanke <50258809+kunalznk@users.noreply.github.com>
2023-07-30 21:57:16 +0530

    skiils added

--- app/components/Tech.js
+++ app/components/Tech.js
@@ -1,55 +1,81 @@
 import Image from "next/image";
 
 export const skills = [
-    {
-      title: "HTML",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  {
+    title: "Javascript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  }, 
+  {
+    title: "Typescript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
     },
     {
-      title: "CSS",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    title: "NodeJS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
     },
     {
-      title: "Javascript",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      title: "ExpressJs",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
     },
     {
-      title: "Sass",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
-    },
-    {
-      title: "TailwindCSS",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
-    },
-    {
-      title: "React",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    },
-    {
-      title: "NextJS",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
-      style: { filter: "invert(1)" },
-    },
-    {
-      title: "Typescript",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    },
-    {
-      title: "NodeJS",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-    },
-    {
-      title: "Git",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-    },
-    {
-      title: "Firebase",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
-    },
-    {
-      title: "Figma",
-      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
-    },
+      title: "MySQL",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+      },
+      {
+        title: "MongoDB",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+        },
+        {
+          title: "Docker",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+          },
+          {
+            title: "Linux",
+            icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+            },
+
+          {
+            title: "Rust",
+            icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg",
+            },
+              {
+                title: "AWS",
+                icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg",
+                },
+                {
+                  title: "Serverless",
+                  icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+                  },
+                  {
+                    title: "Sequelize",
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg",
+                    },
+                    {
+                      title: "NextJS",
+                      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+                      style: { filter: "invert(1)" },
+                      
+                    },
+                    {
+                      title: "React",
+                      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+                    },
+                    {
+                      title: "Redux",
+                      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+                    },
+                    {
+                      title: "TailwindCSS",
+                      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+                    },
+                  {
+                    title: "Material UI",
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+                  },
+                  {
+                    title: "Git",
+                    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+                  }
   ];
 
 const Tech = () => {
@@ -65,14 +91,14 @@ const Tech = () => {
     place-items-center 
     "
     >
-                     {[...skills,...skills].map((item, index) => <div
+                     {skills.map((item, index, arr) => <div
                      key={index}
-                     data-aos="fade-up"
+                     data-aos={ (index / 2) > 2 ? "fade-left" :  "fade-right"}
                      data-aos-duration="700"
                      data-aos-delay={50+ (150*index)}
                      >
-                     <div  className="group sm:border-4 border-slate-300 flex gap-2 justify-center items-center p-4 
-                     rounded-full transition-all hover:bg-secondary hover:text-black hover:shadow-lg hover:scale-125 hover:cursor-pointer
+                     <div  className="group sm:border-2 border-slate-300 flex gap-2 justify-center items-center p-4 
+                     rounded-full transition-all hover:bg-white  text-white hover:text-black hover:shadow-lg hover:scale-125 hover:cursor-pointer
                      "
                      >
                 <div className="">
@@ -81,7 +107,7 @@ const Tech = () => {
           key={index}
           className="w-20 h-20 mx-auto flex items-center flex-col justify-center"
         >
-          <img src={item.icon} className="h-10 w-10 group-hover:animate-bounce"/>
+          <img src={item.icon} className="h-12 w-12 contrast-125	 group-hover:animate-bounce"/>
           <p className="text-xs group-hover:text-black font-bold mt-3 opacity-80">
             {item.title}
           </p>
